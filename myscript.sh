43d8e1dda3f82851e92a7615eab1289027929ad1
Abhitesh Kulkarni <116165162+abhiteshkulkarni@users.noreply.github.com>
2025-04-02 13:29:55 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Welcome to Jenkins Classes"
-course="AWS & DevOps cloud"
+course="AWS & DevOps cloud Engineer"
 echo "We are learning $course by Reyaz sir. This $course is fantastic and mind-blowing. All $course concepts are clear. Explanation of $course at an experienced high level."
